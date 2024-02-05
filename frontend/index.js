1b090b07f4f5755de719982b1af010525b60921a
Aaron Kendell <aaronkendell@gmail.com>
2024-02-05 12:29:34 -0700

    misc commands to get app to work

--- frontend/index.js
+++ frontend/index.js
@@ -42,6 +42,8 @@ app.use(deleteFileRoute);
 app.use(presignedUrlRoute);
 app.use(updateRoute);
 app.use(cors(corsOptions));
+app.use(app.use(express.static(path.join(__dirname, 'client/build'))));
+
 
 app.use(express.static('client/build'));
 app.get('*', (req, res) => {
