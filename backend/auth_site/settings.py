53ba4196dfae8cb81b398f91fea1c37b68a45a6c
Aaron Kendell <aaronkendell@gmail.com>
2024-02-06 13:49:46 -0700

    changed domains to allow commication with frontend

--- backend/auth_site/settings.py
+++ backend/auth_site/settings.py
@@ -28,9 +28,12 @@ environ.Env.read_env()
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['simple-notes-backend-dev.us-east-2.elasticbeanstalk.com', '*']
+ALLOWED_HOSTS = ['http://simple-notes-backend-dev.us-east-2.elasticbeanstalk.com', "http://simple-notes-frontend-dev.us-east-2.elasticbeanstalk.com",]
+# Assuming your second machine has an IP address of 192.168.1.5
+# ALLOWED_HOSTS = ['136.38.137.165', 'localhost', '127.0.0.1', '*']
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -62,7 +65,7 @@ ROOT_URLCONF = 'auth_site.urls'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "simple-notes-frontend-dev.us-east-2.elasticbeanstalk.com",
+    "http://simple-notes-frontend-dev.us-east-2.elasticbeanstalk.com",
 ]
 
 CORS_ALLOW_METHODS = [
