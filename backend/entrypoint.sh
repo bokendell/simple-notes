96c3df0d02a664a1721ddcb60f52c66ac5d3c9bd
Aaron Kendell <aaronkendell@gmail.com>
2025-01-09 02:09:53 -0700

    fixed entrypoint again

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -33,7 +33,7 @@ start_prod_server() {
     collect_static_files
 
     echo_info "Starting Gunicorn server..."
-    exec gunicorn authsite.wsgi:application --bind 0.0.0.0:8000
+    exec gunicorn auth_site.wsgi:application --bind 0.0.0.0:8000
 }
 
 
